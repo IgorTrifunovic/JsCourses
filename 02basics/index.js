b920f79f2bc1ad57cab7f129a749e7a08bf9da0c
IgorTrifunovic <igor13trifunovic@gmail.com>
2022-02-06 14:02:41 +0100

    add 02basics practice material

---
+++ 02basics/index.js
@@ -0,0 +1,20 @@
+
+
+// let name = 'igor'; 
+// let age = 30;
+// let isApprevoed = false;
+// let firstname = undefined;
+// let selectColor = null;
+// console.log(name);
+
+// function pozdrav(osoba){
+//     console.log('Hallo '+osoba+' !')
+// }
+
+// pozdrav('Zeljka')
+
+function square(num){
+    return num * num;
+}
+
+console.log(square(5689))
