911d432aa435c15f96acdba69acb3ab6f8094133
IgorTrifunovic <88130803+IgorTrifunovic@users.noreply.github.com>
2022-03-07 09:59:13 +0100

    Revert 16 update2.2 forth branch2 (#17)
    * add sandbox.js for 05basics (#11)
    
    * add sandbox.js for 05basics
    
    * new commit
    
    * second commit
    
    * Update2.2 forth branch2 (#16)
    
    * creating m.conflict with branch created from main
    
    * second commit
    
    * this branch merge to update2.2ForthBranch
    
    * Revert "Update2.2 forth branch2 (#16)"
    
    This reverts commit da2c7dbeecac40ad4cb1fe0e2fbb7f3f10d05bcf.

---
+++ 05basics/chapter_4_ArrowFn/sandbox.js
@@ -0,0 +1,57 @@
+//                                    YT:   The Net Ninja
+
+//retirning values
+
+// const speak = function(name='luigi', time='night'){
+// console.log(`good ${time} ${name}`);
+// };
+
+// speak('mario', 'morrning');
+// speak(); 
+
+// regular Fn
+// const calcArea = function(radius){
+//     let area = 3.14 * radius **2;
+//     return area
+// };
+
+// shorter
+// const calcArea = function(radius){
+//     return 3.14 * radius **2;
+// };
+
+// arrow Fn
+
+// const calcArea = (radius) => {
+//     return 3.14 * radius**2;
+// };
+
+//shorter arrow
+
+const calcArea = radius => 3.14 * radius**2;                // 0 or multiple parameters ()
+                                            // if we have simple return we can remove {} as the return word
+const area = calcArea(5);
+console.log(area);
+
+
+
+
+const greet = () => 'hello, world';
+
+const greetVar = greet();
+console.log(greetVar);
+
+
+
+const bill = (products, tax) => {
+    let total = 0
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+
+console.log(bill([20, 50, 30], 0.2));
+
+// this is comment for new git commit
+// something for second commit
